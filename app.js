1f5fecb838d3b5bf04038e66c79e92bf3598f855
Vikash <vikash@programmminggeek.in>
2015-06-20 02:35:05 +0530

    Setup app :walking:

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ define(function() {
   console.log("serverApi is loaded");
   return {
     callAjax: function() {
+      alert('callAjax called!');
       console.log('callAjax called!');
     },
     getAllIssues: function() {}
